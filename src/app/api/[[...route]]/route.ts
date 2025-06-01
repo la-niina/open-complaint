85fad5cb5208587b15b7d2ba3517217a17530cf8
la-niina <la.niina.me@gmail.com>
2025-06-01 07:27:02 +0300

    update

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,13 +1,39 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { cache } from "hono/cache";
+import { logger } from "hono/logger";
 import { handle } from "hono/vercel";
+import { languageDetector, LanguageVariables } from "hono/language";
+import { compress } from "hono/compress";
 
-const app = new Hono().basePath("/api");
+const server = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
+server.use("*", cors());
+server.use("*", compress());
+server.use("*", logger());
+server.use(
+  "*",
+  languageDetector({
+    convertDetectedLanguage: (lang) => lang.split("-")[0],
+    supportedLanguages: ["en", "zh", "fr"],
+    fallbackLanguage: "en",
+  })
+);
+server.use(
+  "*",
+  cache({
+    cacheName: "open complaint",
+    cacheControl: "max-age=3600",
+  })
+);
+
+server.get("/hello", (c) => {
+  const lang = c.get("language");
+
   return c.json({
-    message: "Hello Next.js!",
+    message: `Hello Next.js! ${lang}`,
   });
 });
 
-export const GET = handle(app);
-export const POST = handle(app);
+export const GET = handle(server);
+export const POST = handle(server);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { GeistSans as sans } from "geist/font/sans";
+import { GeistMono as mono } from "geist/font/mono";
+import "@/app/globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,11 +16,34 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html
+      lang="en"
+      className={`${sans.variable} ${mono.variable}`}
+      suppressHydrationWarning
+    >
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={"anonymous"}
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@
+          400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className={`antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
